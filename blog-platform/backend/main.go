16ae3f229a661d3dbdead6c07e449ca98b7d2af5
AliasgharHeidari <Aliasgharaliali1213@gmail.com>
2026-08-15 02:17:58 +0330

    feat: added cors for deploy

--- blog-platform/backend/main.go
+++ blog-platform/backend/main.go
@@ -41,7 +41,7 @@ func main() {
 	app.Use(logger.New())
 	app.Use(recover.New())
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:5173,http://localhost:3000",
+		AllowOrigins:     "http://localhost:5173,http://localhost:3000,https://weblog-manager-front.onrender.com",
 		AllowCredentials: true,
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowMethods:     "GET, POST, PUT, DELETE, OPTIONS",
